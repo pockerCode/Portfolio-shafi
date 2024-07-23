edfd2cded8d214120af6f88469f2c52a0c72350a
Mohammed Shafi Mundassery <shafimsm19@gmail.com>
2024-07-24 00:10:02 +0530

    added the microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -64,9 +65,19 @@ export default function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
             {children}
+
             <Navbar />
           </TooltipProvider>
         </ThemeProvider>
+        <Script id="clarity-script" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "nbzeplrnnx");
+          `}
+        </Script>
       </body>
     </html>
   );
